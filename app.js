01fb3f70c81a0c6cd68131e47fb137b217f197c2
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-29 12:08:23 -0700

    bishop can move one space

--- app.js
+++ app.js
@@ -288,6 +288,20 @@ function renderPlayable(position) {
                     }
                 }
             }
+            if (board[position].piece === 'bishop') {
+                displayBoard();
+                const moves = bishop(position);
+
+                for (let move of moves) {
+                    if (move.condition === 'enemy') {
+                        attackButton(position, move.space) ;
+                    }
+
+                    if (move.condition === 'empty') {
+                        moveButton(position, move.space);
+                    }
+                }
+            }
         });
     }
 
@@ -477,6 +491,47 @@ function king(position) {
     }
     
 } 
+
+function bishop(position) {
+    let moves = [];
+
+    const coords = stringToCoords(position);
+    let x = coords[0];
+    let y = coords[1];
+
+    if(inRange(y+1)) {
+        
+        
+        if(inRange(x-1)) {
+            const test = coordsToString([x-1, y+1]);
+            if (inspectSpace(test)) {
+                moves.push(inspectSpace(test));
+            }
+        }
+        if(inRange(x+1)) {
+            const test = coordsToString([x+1, y+1]);
+            if (inspectSpace(test)) {    
+                moves.push(inspectSpace(test));
+            }
+        }
+        
+    }
+    if (inRange(y-1)) {
+        if (inRange(x-1)) {
+            const test = coordsToString([x-1, y-1]);
+            if (inspectSpace(test)) {
+                moves.push(inspectSpace(test));
+            }
+        }
+        if (inRange(x+1)) {
+            const test = coordsToString([x+1, y-1]);
+                if (inspectSpace(test)) {
+                    moves.push(inspectSpace(test));
+                }
+        }    
+    }
+    return moves;
+}
 
 // this just makes sure that any coordinate we look at is between 0 and 8, i.e. on the board
 function inRange(number) {
